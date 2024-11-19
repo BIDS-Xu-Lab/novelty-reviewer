98d6b7a989475c15f92a11b28f24d38db14acb9b
Huan He <140455389+huanhe4096@users.noreply.github.com>
2024-11-19 12:43:05 -0500

    add relative path

--- web/vite.config.js
+++ web/vite.config.js
@@ -4,13 +4,16 @@ import Components from 'unplugin-vue-components/vite';
 import {PrimeVueResolver} from '@primevue/auto-import-resolver';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    Components({
-      resolvers: [
-        PrimeVueResolver()
-      ]
-    }),
-  ],
-})
+export default ({ mode }) => {
+  return defineConfig({
+    base: './',
+    plugins: [
+      vue(),
+      Components({
+        resolvers: [
+          PrimeVueResolver()
+        ]
+      }),
+    ],
+  })
+}
